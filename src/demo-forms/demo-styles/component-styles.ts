8de79e85cf0933d722f17a35dbad2290bdcab97b
Sam Tanner <samuel.m.s.tanner@gmail.com>
2023-11-10 18:35:32 -0800

    gets tailwind styling working

--- src/demo-forms/demo-styles/component-styles.ts
+++ src/demo-forms/demo-styles/component-styles.ts
@@ -1,7 +1,7 @@
 export const defaultLabelStyles = "ml-1 text-sm font-bold"
 
 export const defaultInputStyles =
-  "p-2 w-80 rounded-md border-4 ring-2 ring-zinc-400 border-maak-black aria-hidden focus:outline-none focus:ring-blue-500"
+  "p-2 w-80 rounded-md border-4 ring-2 ring-zinc-400 border-maak-black focus:border-maak-black focus:ring-blue-500 focus:ring-2 fade-in-out"
 
 export const checkboxStyling =
-  "dark:checked:focus:bg-zinc-500 h-5 w-5 rounded-sm border-2 border-zinc-500 bg-zinc-100 ring-0 checked:border-zinc-500 checked:bg-zinc-500 hover:bg-zinc-200 hover:checked:bg-zinc-600 focus:ring-0 focus:ring-offset-0 checked:focus:bg-zinc-500 dark:border-zinc-500 dark:bg-zinc-800 dark:checked:bg-zinc-500 dark:hover:bg-zinc-700 dark:hover:checked:bg-zinc-700"
+  "dark:checked:focus:bg-zinc-500 h-5 w-5 rounded-sm border-2 border-zinc-500 bg-zinc-100 ring-0 checked:border-zinc-500 checked:bg-zinc-500 hover:bg-zinc-200 hover:checked:bg-zinc-600 focus:ring-0 focus:ring-offset-0 checked:focus:bg-zinc-500 dark:border-zinc-500 dark:bg-zinc-800 dark:checked:bg-zinc-500 dark:hover:bg-zinc-700 dark:hover:checked:bg-zinc-700 fade-in-out"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const plugin = require("tailwindcss/plugin")
+
+
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
@@ -9,5 +12,19 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    plugin(function ({ addUtilities }) {
+      /** @type {import('@types/tailwindcss').Utility} */
+      const utilities = {
+        ".fade-in-out": {
+          transition: "all 200ms ease-in-out",
+        },
+      }
+
+      addUtilities(utilities, ["responsive", "hover"]) // Variants
+    }),
+  ],
 }
